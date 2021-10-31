6159d4009ca43271e30f47fcc6ee19497153fc48
sreemonta20 <sbhowmikcse08@gmail.com>
2021-10-31 11:18:28 +0600

    changes 1

---
+++ src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+// import { conversions } from '../../src/app/shared/utilities/conversions';
+//providers: [conversions],
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent  {
+  title = 'pfsbhowmikv1';
+  // message: 'Welcome';
+
+}
+// export class AppComponent  implements OnInit {
+//   title = 'pfsbhowmikv1';
+//     constructor(private router: Router) {
+
+//     }
+
+//     ngOnInit(): void {
+
+//     }
+
+
+//   linktoContact(){
+//     this.router.navigateByUrl('/portfolio');
+//   }
+// }
